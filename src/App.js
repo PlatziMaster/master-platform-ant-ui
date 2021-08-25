380161e43df9e509a5567c97f13a7c214fd08006
Nicolas Molina Monroy <hi@nicobytes.com>
2021-08-24 20:29:23 -0400

    :tada: Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import DemoPage from './components/pages/demo.page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={DemoPage}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/demo.page.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+function DemoPage() {
+  return (
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default DemoPage;
